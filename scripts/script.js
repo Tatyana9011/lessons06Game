9b21a332119c39f7d954c4e1fb78cc28dfd7b286
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-09 02:51:58 +0300

    lessons06 refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -5,63 +5,52 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let closeGame = function (userNumber) {
-  if (userNumber === null || !userNumber) {
-    alert('Игра окончена');
-  }
-};
-
 function validationNumber(userNumber) {
   let randomNumber = 15;
   let counter = 0;
 
   (function valueNumber() {
     if (counter >= 9) {
-      console.log(counter);
-        let newGame = confirm("Попытки закончились, хотите сыграть еще?");
-         if (!newGame) {
-          return  closeGame(newGame);
-          }
+      let newGame = confirm("Попытки закончились, хотите сыграть еще?");
+        if (!newGame) {
+        return  alert('Игра окончена');
+        }
       counter = 0;
-      return validationNumber();
+      return validationNumber(userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток'));
+    }
+
+    if (userNumber === null ) {
+    return alert('Игра окончена');
     }
 
     if (!isNumber(userNumber)) {
       userNumber = prompt(' Введите число!');
-      if (userNumber === null) {
-       return closeGame(userNumber);
-      }
+      
       valueNumber();
-    }else if (userNumber > 100) {
+    } else if (userNumber > 100) {
       userNumber = prompt(
         `Вы ввели число больше 100. Попробуйте еще раз!`);
-      if (userNumber === null) {
-       return closeGame(userNumber);
-      }
+
       valueNumber();
     } else if (userNumber > randomNumber && userNumber !== 0) {
       counter++;
       userNumber = prompt(
         `Загаданное число меньше. Введите другое число. У вас осталось ${10 - counter} попыток`);
-      if (userNumber === null) {
-       return closeGame(userNumber);
-      }
+
       valueNumber();
     }else if (userNumber < randomNumber && userNumber!==0) {
       counter++;
       userNumber = prompt(
         `Загаданное число больше. Введите другое число. У вас осталось ${10 - counter} попыток`);
-      if (userNumber === null) {
-       return closeGame();
-      }
+      
       valueNumber();
     }else if (+userNumber===randomNumber){
       let newGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
         if (!newGame) {
-          return closeGame(newGame); 
+          return alert('Игра окончена');
         }
       counter = 0;
-     return validationNumber();
+     return validationNumber(userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток'));
     }
   }());
 }
@@ -70,7 +59,7 @@ function validationNumber(userNumber) {
 buttonGame.addEventListener('click', () => {
   let userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток');
   if (userNumber === null) {
-   return closeGame(userNumber);
+   return alert('Игра окончена');
   }
   validationNumber(userNumber);
 });
