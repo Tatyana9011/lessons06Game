52d07544a5db8c940439a6aa6974ec2059545c17
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-04-10 15:19:14 +0300

    lessons06Maksim
    git commit -a -m lessons06Maksim
    
    git commit -a -m lessons06Maksim

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,83 @@
 "use strict";
-const buttonGame = document.querySelector('.button-game' );
+const getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // это необязательный вариант защиты если перепутать мин и макс местами
+ // min = Math.min(min, max); 
+ // max = Math.max(min, max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function validationNumber(userNumber) {
-  let randomNumber = 15;
-  let counter = 0;
+const getNumber = function (str) {
+  const numUser = prompt(str);
 
-  (function valueNumber() {
-    if (counter >= 9) {
-      let newGame = confirm("Попытки закончились, хотите сыграть еще?");
-        if (!newGame) {
-        return  alert('Игра окончена');
-        }
-      counter = 0;
-      return validationNumber(userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток'));
-    }
-
-    if (userNumber === null ) {
-    return alert('Игра окончена');
-    }
-
-    if (!isNumber(userNumber)) {
-      userNumber = prompt(' Введите число!');
-      
-      valueNumber();
-    } else if (userNumber > 100) {
-      userNumber = prompt(
-        `Вы ввели число больше 100. Попробуйте еще раз!`);
-
-      valueNumber();
-    } else if (userNumber > randomNumber && userNumber !== 0) {
-      counter++;
-      userNumber = prompt(
-        `Загаданное число меньше. Введите другое число. У вас осталось ${10 - counter} попыток`);
-
-      valueNumber();
-    }else if (userNumber < randomNumber && userNumber!==0) {
-      counter++;
-      userNumber = prompt(
-        `Загаданное число больше. Введите другое число. У вас осталось ${10 - counter} попыток`);
-      
-      valueNumber();
-    }else if (+userNumber===randomNumber){
-      let newGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-        if (!newGame) {
-          return alert('Игра окончена');
-        }
-      counter = 0;
-     return validationNumber(userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток'));
-    }
-  }());
-}
-
-
-buttonGame.addEventListener('click', () => {
-  let userNumber = prompt('Угадай число от 1 до 100 у вас 10 попыток');
-  if (userNumber === null) {
-   return alert('Игра окончена');
+  if (numUser === null) {
+    return null;
   }
-  validationNumber(userNumber);
-});
+  if (isNumber(numUser)) {
+    return +numUser;
+  }
+  alert('Кажется вы забыли ввести число Попробуйте еще раз!');
+  getNumber(str);
+
+};
+// будет возвращать ф-цию которая будет увеличивать counter и конешно же ф-ция будет возвр counter
+const getCounter = function () {
+  let counter = 0;
+  return function () {
+    return counter++;
+  };
+};
+
+const gameBot = function (attemps, min, max) {
+  attemps = attemps || 10;
+  min = min || 0;
+  max = max || 100;
+
+  const random = getRandomInt(min, max);
+  const counter = getCounter();
+
+  function checkNumber() {
+
+    const count = counter();
+
+    if (count < attemps) {
+      const number = getNumber('Попробуйте угадать число от ' + min + ' до ' + max);
+      if (number === null) return alert('Досвидос Амиго!');
+      
+      if (number > random) {
+        alert('Загаданное число меньше чем ваше!');
+        //когда рекурсия пойдет в обратную сторону что бы наша функция не пошла дальше выполняться
+        return checkNumber();
+      }
+      if (number < random) {
+        alert('Загаданное число больше чем ваше!');
+        return checkNumber();
+      }
+      if (number === random) {
+        alert('Молодец угадал!');
+        return checkNumber();
+      }
+    } else {
+      alert('Количество попыток закончилось! Было загаданно число' + random);
+    }
+
+    const questAC = confirm('Хотите сыграть еще');
+
+    if (questAC) {
+      alert('Отлично начинаем!');
+      gameBot(attemps, min, max);
+    } else {
+      alert('Спасибо за игру еще увидимся!');
+    }
+  }
+
+  checkNumber();
+
+};
+
+gameBot(20, 0, 10);
